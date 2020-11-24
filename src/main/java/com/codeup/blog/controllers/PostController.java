f76e926968924dd7035f091574ed17a9b0d4b7a9
Roberto <roberto.cuyar44@gmail.com>
2020-11-24 14:07:59 -0600

    Finished View Exercises

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,22 +1,40 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String postIndex (){
-        return "posts index page";
+    public String postIndex (Model model){
+        ArrayList<Post> postList = new ArrayList<>();
+        postList.add(new Post(
+                "My favorite snack",
+                "We all need to eat popcorn. The main reason why is because popcorn is a quick to feel full even though we don't eat a whole lot of food overall. Popcorn should rain from the skies."
+        ));
+        postList.add(new Post(
+                "What to do",
+                "Well, we really don't know what to do at times. Maybe we should just code and think it is okay. Or maybe...just maybe...we can just see if this works."
+        ));
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postView(@PathVariable long id){
-        return "view an individual post.";
+    public String postView(@PathVariable long id, Model model){
+        Post post = new Post(
+                "My favorite snack",
+                "We all need to eat popcorn. The main reason why is because popcorn is a quick to feel full even though we don't eat a whole lot of food overall. Popcorn should rain from the skies."
+        );
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
