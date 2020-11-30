e9495c950d16ed8e744377c86ca8003043913205
Roberto <roberto.cuyar44@gmail.com>
2020-11-30 09:46:22 -0600

    Repositories and JPA exercise parts 1-4 completed

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,9 +1,20 @@
 package com.codeup.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
-    private String title;
-    private String body;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column (nullable = false, length = 100)
+    private String title;
+
+    @Column (nullable = false, columnDefinition = "TEXT")
+    private String body;
+
 
     public Post (){};
 
