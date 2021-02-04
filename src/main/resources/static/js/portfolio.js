7ac08bbebbc4f972e5fbfe3d3f84959baef545db
Roberto <roberto.cuyar44@gmail.com>
2021-02-04 17:12:19 -0600

    Tooltip changes to tech icons and complete screenshots of each app.

--- src/main/resources/static/js/portfolio.js
+++ src/main/resources/static/js/portfolio.js
@@ -1,26 +1,4 @@
-const para = document.createElement("p");
-
-const iconChange = (selector, text)=> {
-    const htmlIcon = document.querySelectorAll(selector);
-
-    htmlIcon.forEach((icon) => {
-        icon.addEventListener("mouseover", () => {
-            icon.firstElementChild.classList.add("d-none");
-            icon.appendChild(para)
-            para.innerText = text;
-        })
-        icon.addEventListener("mouseleave", () => {
-            icon.firstElementChild.classList.remove("d-none");
-            icon.removeChild(para);
-        })
-    })
-}
-
-iconChange(".html-parent", "HTML5");
-iconChange(".css-parent", "CSS");
-iconChange(".js-parent", "Java Script");
-iconChange(".boot-parent", "Bootstrap");
-iconChange(".spring-parent", "Spring Boot");
-iconChange(".react-parent", "React.js");
-iconChange(".java-parent", "Java");
-
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
